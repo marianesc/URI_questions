c7affbac6c86da407a8c5f2b52e152e7d17b722d
Ana <analiviab94@gmail.com>
2022-10-13 09:58:16 -0300

    adding 1001 js and py

---
+++ 1001.js
@@ -0,0 +1,9 @@
+//Extremamente Básico
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var A = parseInt(lines.shift())
+var B = parseInt(lines.shift())
+
+console.log('X = ' + (A+B))
---
+++ 1001.py
@@ -0,0 +1,7 @@
+#Extremamente Básico
+
+a = int(input())
+b = int(input())
+
+x = a + b
+print("X = {result}".format(result = x))
